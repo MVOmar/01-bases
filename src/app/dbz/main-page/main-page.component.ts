4e2d8282039d2d6c58e0e440b2d03f698c0ca25c
Dev-Vilchis <mtz.vilchisomar@gmail.com>
2023-02-07 16:10:34 -0600

    fin de seccion 5

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,47 +1,24 @@
 import { Component } from '@angular/core';
+import { DbzService } from '../services/dbz.service';
 
 
 interface Personaje {
-  nombre : string;
-  poder : number;
+  nombre: string;
+  poder: number;
 }
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre:'Vegeta',
-      poder: 7500
-    }
-  ];
-
   nuevo: Personaje = {
-    nombre: '',
-    poder: 0
+    nombre: 'Maestro Roshi',
+    poder: 1000
   }
 
-  agregar() {
-    if(this.nuevo.nombre.trim().length===0){return;}
-
-    this.personajes.push(this.nuevo)
-    this.nuevo={
-      nombre:'',
-      poder:0
-  }
-
-    console.log(this.nuevo);
-
-  }
-
-  }
+  constructor( private dbzService: DbzService){}
+}
 
 
